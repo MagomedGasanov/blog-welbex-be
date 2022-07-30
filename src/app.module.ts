fd55756c9d1b11314a30b6228de8c327dec5e650
MagomedGasanov <magomed-gasanov1996@yandex.ru>
2022-07-30 20:24:01 +0300

    Setup folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env' }), UsersModule, AuthModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/core/database/database.config.ts
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+import { SequelizeOptions } from 'sequelize-typescript';
+
+dotenv.config();
+
+export const databaseConfig: Record<
+    'development' | 'test' | 'production',
+    SequelizeOptions
+> = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME_DEVELOPMENT,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        dialect: 'postgres',
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME_TEST,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        dialect: 'postgres',
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME_PRODUCTION,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    },
+};
