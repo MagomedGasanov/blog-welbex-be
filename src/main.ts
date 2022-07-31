4e59cb1f110e6e678f6f1bc3393b22daadacf1fe
MagomedGasanov <magomed-gasanov1996@yandex.ru>
2022-07-31 19:38:36 +0300

    Validation pipe + globalPrefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api/v1');
+
+    app.useGlobalPipes(new ValidationPipe());
+
     await app.listen(PORT, () => console.log(`Server started. PORT: ${PORT}`));
 }
 bootstrap();
